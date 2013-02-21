fe08423b36cab0ba67179dfde9da4f11da3e4bd3
AbelCheng_cp <SND\AbelCheng_cp>
2013-02-21 04:07:03 +0000

    Creating blank solution ...

---
+++ T4SQLTemplateLibrary/ConsoleTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T4SQL.ConsoleTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
