24227b2d47d3f921f2a24c84d35034870ac7c44a
Abel Cheng <abelcys@gmail.com>
2013-06-26 03:50:17 +0000

    Prepare test (sample) schema

--- T4SQLTemplateLibrary/Databases/SqlServer/Test/Tables/test.sample_properties.sql
+++ T4SQLTemplateLibrary/Databases/SqlServer/Test/Tables/test.sample_properties.sql
@@ -1,4 +1,16 @@
-﻿CREATE SCHEMA [T4SQL];
+﻿CREATE TABLE test.sample_properties
+(
+	WORKITEM_NAME	NVARCHAR(32)	NOT NULL,
+	PROPERTY_NAME	NVARCHAR(64)	NOT NULL,
+	STRING_VALUE	NVARCHAR(4000)	NOT NULL,
+	LINK_STATE		NVARCHAR(256),
+
+	CONSTRAINT PK_SEED_PROPERTY PRIMARY KEY (WORKITEM_NAME, PROPERTY_NAME),
+	CONSTRAINT FK_SEED_PROPERTY_WORKITEM FOREIGN KEY (WORKITEM_NAME)
+		REFERENCES test.sample_workspace(WORKITEM_NAME)
+		ON UPDATE  CASCADE 
+		ON DELETE  CASCADE
+);
 
 ----------------------------------------------------------------------------------------------------
 --
@@ -9,7 +21,7 @@
 --	You must not remove this notice, or any other, from this software.
 --
 --	Original Author:	Abel Cheng <abelcys@gmail.com>
---	Created Date:		‎February ‎21, ‎2013, ‏‎11:23:38 PM
+--	Created Date:		June ‎25, ‎2013, ‏‎12:23:54 AM
 --	Primary Host:		http://t4sql.codeplex.com
 --	Change Log:
 --	Author				Date			Comment
