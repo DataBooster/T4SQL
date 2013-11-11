44a40eaabf83298f086e43ea32ebee0ab0e2742d
AbelCheng_cp <SND\AbelCheng_cp>
2013-11-11 05:23:17 +0000

    Preparing Test Schema

---
+++ T4SQLTemplateLibrary/Databases/Oracle/Test/Scripts/2-InitData/1-CreateSampleWorkspace.sql
@@ -0,0 +1,5 @@
+BEGIN
+	IF NOT T4SQL.META.EXISTS_TABLE('test.sample_workspace') AND NOT T4SQL.META.EXISTS_TABLE('test.sample_properties') THEN
+		T4SQL.META.CREATE_WORKSPACE('test.sample_workspace', 'test.sample_properties', 'Sample Workspace', 'test');
+	END IF;
+END;
