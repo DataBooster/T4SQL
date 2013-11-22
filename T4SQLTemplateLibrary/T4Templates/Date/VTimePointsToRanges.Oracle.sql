79b2f1e9c15b6fd7a659a8fbf36608e1dfd1febe
AbelCheng_cp <SND\AbelCheng_cp>
2013-11-22 05:23:54 +0000

    VTimePointsToRanges.Oracle.sql and VNaviForeignKey.Oracle.sql

--- T4SQLTemplateLibrary/T4Templates/Date/VTimePointsToRanges.Oracle.sql
+++ T4SQLTemplateLibrary/T4Templates/Date/VTimePointsToRanges.Oracle.sql
@@ -12,3 +12,24 @@ FROM
 	<#= SourceView #>
 
 WITH READ ONLY;
+
+----------------------------------------------------------------------------------------------------
+--
+--	Copyright 2013 Abel Cheng
+--	This source code is subject to terms and conditions of the Apache License, Version 2.0.
+--	See http://www.apache.org/licenses/LICENSE-2.0.
+--	All other rights reserved.
+--	You must not remove this notice, or any other, from this software.
+--
+--	Original Author:	Abel Cheng <abelcys@gmail.com>
+--	Created Date:		‎May ‎20, ‎2013, ‏‎12:00:44 AM
+--	Primary Host:		http://t4sql.codeplex.com
+--	Change Log:
+--	Author				Date			Comment
+--
+--
+--
+--
+--	(Keep code clean)
+--
+----------------------------------------------------------------------------------------------------
